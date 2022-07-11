9c6b1d65f15cba9e1c6edc68e21d08ebe69196d4
dingshuaige <281970621@qq.com>
2022-07-11 16:12:55 +0800

    用户账变提交

---
+++ src/main/java/my/fast/admin/app/service/AppMemberAccountChangeControllerService.java
@@ -0,0 +1,45 @@
+package my.fast.admin.app.service;
+
+import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import my.fast.admin.app.entity.AppMemberAccountChange;
+
+/**
+ * TODO
+ *
+ * @author cgkj@cg.cn
+ * @version V1.0
+ * @since 2022/7/10 10:38
+ */
+public interface AppMemberAccountChangeControllerService {
+
+    /**
+     * 获取所有用户账变记录
+     */
+    List<AppMemberAccountChange> listAll();
+
+    /**
+     * 分页查询
+     */
+    List<AppMemberAccountChange> listAccountChange(AppMemberAccountChange appMemberAccountChange, Integer pageNum, Integer pageSize);
+
+    /**
+     * 删除用户账变
+     */
+    int deleteAccountChangeById(Long id);
+
+    /**
+     * 创建用户账变
+     */
+    int createAccountChange(AppMemberAccountChange appMemberAccountChange);
+
+    /**
+     * 修改用户账变
+     */
+    @Transactional
+    int updateAccountChange(Long id,AppMemberAccountChange appMemberAccountChange);
+    
+    
+}
---
+++ src/main/java/my/fast/admin/app/service/impl/AppMemberAccountChangeControllerServiceImpl.java
@@ -0,0 +1,66 @@
+package my.fast.admin.app.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.pagehelper.PageHelper;
+
+import my.fast.admin.app.entity.AppMemberAccountChange;
+import my.fast.admin.app.entity.AppMemberAccountChangeExample;
+import my.fast.admin.app.mapper.AppMemberAccountChangeMapper;
+import my.fast.admin.app.service.AppMemberAccountChangeControllerService;
+
+/**
+ * TODO
+ *
+ * @author cgkj@cg.cn
+ * @version V1.0
+ * @since 2022/7/10 10:40
+ */
+@Service
+public class AppMemberAccountChangeControllerServiceImpl implements AppMemberAccountChangeControllerService {
+
+    @Autowired
+    private AppMemberAccountChangeMapper appMemberAccountChangeMapper;
+
+    @Override
+    public List<AppMemberAccountChange> listAll() {
+        return appMemberAccountChangeMapper.selectByExample(new AppMemberAccountChangeExample());
+    }
+
+    @Override
+    public List<AppMemberAccountChange> listAccountChange(AppMemberAccountChange appAccountChange, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        AppMemberAccountChangeExample appGoodsExample = new AppMemberAccountChangeExample();
+//        AppConveyExample.Criteria criteria = appGoodsExample.createCriteria();
+//        if (!StringUtils.isEmpty(appConvey.get)) {
+//            criteria.andGoodsNameLike("%" + goodsName + "%");
+//        }
+        
+        return appMemberAccountChangeMapper.selectByExample(appGoodsExample);
+    }
+
+    @Override
+    public int deleteAccountChangeById(Long id) {
+        return appMemberAccountChangeMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public int createAccountChange(AppMemberAccountChange appAccountChangeParam) {
+    	AppMemberAccountChange appMemberAccountChange = new AppMemberAccountChange();
+        BeanUtils.copyProperties(appAccountChangeParam, appMemberAccountChange);
+        return appMemberAccountChangeMapper.insertSelective(appMemberAccountChange);
+    }
+
+    @Override
+    public int updateAccountChange(Long id, AppMemberAccountChange appAccountChangeParam) {
+    	AppMemberAccountChange appMemberAccountChange = new AppMemberAccountChange();
+        BeanUtils.copyProperties(appAccountChangeParam, appMemberAccountChange);
+        appMemberAccountChange.setId(id);
+        return appMemberAccountChangeMapper.updateByPrimaryKeySelective(appMemberAccountChange);
+
+    }
+}
