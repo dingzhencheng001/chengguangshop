0452b8ec9c45e8c420461520776def20d9284e9e
itfeiyue <5519157+itfeiyue@user.noreply.gitee.com>
2022-07-15 14:48:59 +0800

    swagger配置修改

--- src/main/java/my/fast/admin/app/config/SwaggerConfig.java
+++ src/main/java/my/fast/admin/app/config/SwaggerConfig.java
@@ -8,7 +8,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Swagger API文档相关配置
- * Created by macro on 2018/4/26.
+ * Created by cg on 2020/7/5.
  */
 @Configuration
 @EnableSwagger2
@@ -17,12 +17,12 @@ public class SwaggerConfig extends BaseSwaggerConfig {
     @Override
     public SwaggerProperties swaggerProperties() {
         return SwaggerProperties.builder()
-                .apiBasePackage("com.macro.mall.controller")
-                .title("mall后台系统")
-                .description("mall后台相关接口文档")
-                .contactName("macro")
+                .apiBasePackage("my.fast.admin.app.controller")
+                .title("橙光科技")
+                .description("相关接口文档")
+                .contactName("cg")
                 .version("1.0")
-                .enableSecurity(true)
+                .enableSecurity(false)
                 .build();
     }
 }
